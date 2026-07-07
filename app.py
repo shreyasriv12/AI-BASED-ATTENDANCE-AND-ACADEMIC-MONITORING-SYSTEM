ea38cb73bc93a20cac69346db8770679f92ab2f0
shreyasriv12 <146077117+shreyasriv12@users.noreply.github.com>
2026-07-07 20:32:25 +0530

    Delete qr_attendance route from app.py
    Removed qr_attendance route and its functionality.

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
-from qr_scanner import scan_qr
-from attendance import mark_attendance
-
-@app.route("/qr_attendance")
-def qr_attendance():
-    student_name = scan_qr()
-
-    if student_name:
-        mark_attendance(student_name)
-        return {"status": f"{student_name} Attendance Marked ✅"}
-
-    return {"status": "QR Failed ❌"}
-    from flask import Flask
+from flask import Flask
 from qr_scanner import scan_qr
 from attendance import mark_attendance
 
