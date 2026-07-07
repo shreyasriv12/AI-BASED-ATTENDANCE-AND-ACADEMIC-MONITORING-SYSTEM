a8332bb5fee37fbd3f3ce983aac2a72024d994bd
shreyasriv12 <146077117+shreyasriv12@users.noreply.github.com>
2026-07-07 20:24:41 +0530

    Implement attendance marking feature
    Added attendance marking functionality to log student attendance in a CSV file.

--- attendance.py
+++ attendance.py
@@ -3,3 +3,27 @@ from utils.email_service import send_email
 def check_and_alert(name, email, attendance_percent):
     if attendance_percent < 75:
         send_email(email, name)
+        import csv
+import os
+from datetime import datetime
+
+FILE_NAME = "attendance.csv"
+
+def mark_attendance(student_name):
+    file_exists = os.path.isfile(FILE_NAME)
+
+    with open(FILE_NAME, "a", newline="") as file:
+        writer = csv.writer(file)
+
+        if not file_exists:
+            writer.writerow(["Name", "Date", "Time"])
+
+        now = datetime.now()
+
+        writer.writerow([
+            student_name,
+            now.strftime("%d-%m-%Y"),
+            now.strftime("%H:%M:%S")
+        ])
+
+    return True
